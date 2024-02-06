5cb2e07f963a4f165cc3f69db2164c5224723176
miheuer <68270551+miheuer@users.noreply.github.com>
2024-02-06 15:43:07 +0100

    Update abfrage.sh

--- abfrage.sh
+++ abfrage.sh
@@ -2,4 +2,4 @@
 for i in $(cat host.txt);
 #do /Users/miheuer/svn/vmware/rz int $i |grep -e "Bezeichnung" -e "Ansprechpartner" -e "Projekt";
 do rz int $i |grep -e "Bezeichnung" -e "Ansprechpartner" -e "Projekt";
- done
+done
